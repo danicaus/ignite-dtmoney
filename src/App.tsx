1d3a91b21c01477299c821eee28882982e848478
Daniela Caus <danicaus.br@gmail.com>
2021-07-29 23:57:53 -0300

    Create Modal component using reat-modal library

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from './components/Header'
 import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+import Modal from 'react-modal'
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
 
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <GlobalStyle/>
-      <Header/>
-      <Dashboard/>
+      <GlobalStyle />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <Dashboard />
     </>
   );
 }
