8efcd46f281a804e6d85f5a345cc9dbcbd5d3fa3
Daniela Caus <danicaus.br@gmail.com>
2021-07-30 02:26:04 -0300

    Styles income and outcome button based on state

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal'
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioButton } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [ type, setType ] = useState('deposit')
+    
     return (
         <Modal
             isOpen={isOpen}
@@ -33,15 +36,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 />
 
                 <TransactionTypeContainer>
-                    <button type="button">
+                    <RadioButton 
+                        type="button" 
+                        onClick={() => {setType('deposit');}}
+                        isActive={type === 'deposit'}
+                    >
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
-                    </button>
+                    </RadioButton>
                     
-                    <button type="button">
+                    <RadioButton 
+                        type="button" 
+                        onClick={() => {setType('withdraw');}}
+                        isActive={type === 'withdraw'}
+                    >
                         <img src={outcomeImg} alt="Entrada"/>
                         <span>Saída</span>
-                    </button>
+                    </RadioButton>
 
                 </TransactionTypeContainer>
 
