1b19dcaced60fe8dd3aad9891a69a79cda58bb09
Daniela Caus <danicaus.br@gmail.com>
2021-08-03 19:55:47 -0300

    Transform TransactionsContext component on a hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { useState } from "react";
 import Modal from 'react-modal'
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root')
 
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from "react"
-import { api } from "./services/api";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react"
+import { api } from "../services/api";
 
 interface Transaction {
     id: number;
@@ -21,7 +21,7 @@ interface TransactionsContextData {
     createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
 
 export function TransactionsProvider({children}: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -48,4 +48,10 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             {children}
         </TransactionsContext.Provider>
     )
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
 }
